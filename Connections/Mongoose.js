e195dbd7e49e9009b2d67e9e606928e11bb7206d
Shubham Suryawanshi <suryashubham52@gmail.com>
2021-08-12 03:47:30 +0530

    Updates

--- Connections/Mongoose.js
+++ Connections/Mongoose.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose");
 const config = require("../Config/config");
 const logger = require("../Config/logger");
 
-mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
-  logger.info("Connected to MongoDB");
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  });
